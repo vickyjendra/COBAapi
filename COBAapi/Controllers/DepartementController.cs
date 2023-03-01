e26d504884cdf5bb7690ad5a1bc6177f1b885b76
Vicky Jendra Nugraha Danur <VickyJendraNugraha.Danur@Mattel.com>
2023-03-01 09:49:46 +0700

    Add project files.

---
+++ COBAapi/Controllers/DepartementController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace COBAapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartementController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DepartementController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select * from Department";
+            DataTable table = new DataTable();
+            string sqlData = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlData))
+            {
+                myCon.Open();
+                using (SqlCommand cmd = new SqlCommand(query, myCon))
+                {
+                    myReader = cmd.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+    }
+}
---
+++ COBAapi/Models/Departement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COBAapi.Models
+{
+    public class Departement
+    {
+        public int DepartementID { get; set; }
+        public string Name { get; set; }
+    }
+}
